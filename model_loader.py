449933cecccaf80be14d890647a841ca6a92a1ed
HOSHIK RANA <hoshikrana@users.noreply.huggingface.co>
2025-08-22 12:07:55 +0000

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -68,7 +68,7 @@ class StableDiffusionEngine:
             decoder_model.load_state_dict(decoder_weights)
             diffusion_model.load_state_dict(diffusion_weights)
 
-            tokenizer = CLIPTokenizer.from_pretrained("hoshikrana/stable_diffusion_image_generation_v1/tokenizer")
+            tokenizer = CLIPTokenizer.from_pretrained("hoshikrana/stable_diffusion_image_generation_v1")
 
             print(f"Tokenizer loaded from repo {self.repo_id}")
 
